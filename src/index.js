415c6eeda1331f02d2f7c008bdbaccf378692f06
Chung-Yi Chi <chung-yi@artsymail.com>
2016-06-06 18:11:30 -0400

    Expose ArtworQuickEdit component.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as ArtworkAvailability } from './artwork_availability';
 export { default as ArtworkPriceVisibility } from './artwork_price_visibility';
+export { default as ArtworkPriceQuickEdit } from './artwork_price_quick_edit';
